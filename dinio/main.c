844892f3d6958cb0def003efa1832f6c5ba3b09b
Naoki YAMAMOTO <yamamoto@val.co.jp>
2019-10-08 10:09:44 +0900

    Initial Commit

---
+++ dinio/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  dinio
+//
+//  Created by YAMAMOTO Naoki on 2019/10/08.
+//  Copyright © 2019 YAMAMOTO Naoki. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
